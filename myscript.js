b05051cfefde8e8789f8854bd7a705e24c4c1b32
May Kosaka <may.kosaka@gmail.com>
2016-04-20 20:21:29 -0700

    first commit

---
+++ myscript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+   document.getElementById("about_content").innerHTML = "JS test Paragraph. The JS code for myFunction is stored in an external file called "myScript.js".";
+}
